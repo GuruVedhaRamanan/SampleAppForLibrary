9cf73ff8bb327f5e8c43fab3db43afcabbfa9fa1
Guru VedhaRamanan <guru@breezeware.net>
2020-09-01 15:21:20 +0530

    Added class file in llib

---
+++ oAuthUtil/src/main/java/net/breezeware/oauthutil/SampleLib.java
@@ -0,0 +1,12 @@
+package net.breezeware.oauthutil;
+
+import android.util.Log;
+
+public class SampleLib {
+
+    public String tag ="SAMPLE LIB";
+
+    public void printMessage(String message){
+        Log.d(tag,message);
+    }
+}
